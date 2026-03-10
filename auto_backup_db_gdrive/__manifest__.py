372ac970c772d6e62d842eb8a42546f660e2f272
GunasekarnRagul <raguloffic@gamil.com>
2026-03-10 19:55:44 +0530

    c

---
+++ auto_backup_db_gdrive/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Auto Backup Database to Google Drive',
+    'version': '18.0.1.0.0',
+    'category': 'Administration',
+    'summary': 'Authenticate and backup Odoo database to Google Drive',
+    'description': """
+        This module allows you to authenticate with Google Drive and 
+        configure a folder for database backups.
+    """,
+    'author': 'Ragul G',
+    'depends': ['base_setup'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/ir_cron_data.xml',
+        'views/res_config_settings_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
