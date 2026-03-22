d8458f945a4231e7532436d09724ddeebae1cdcc
GunasekarnRagul <raguloffic@gamil.com>
2026-03-22 14:12:42 +0530

    first commit

--- auto_backup_db_cloud/models/cloud_backup_db.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-
-class CloudBackupDb(models.Model):
-    _name = 'cloud.backup.db'
-    _description = 'Cloud Backup Database'
-
-    name = fields.Char(string='Database Name', required=True)
