1a90d40289c3e82b504c7cf35a95b37dd48918ad
GunasekarnRagul <raguloffic@gamil.com>
2026-03-22 15:36:58 +0530

    first commit

--- auto_backup_db_dropbox/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import dropbox_backup_db
-from . import res_config_settings
--- auto_backup_db_dropbox/models/dropbox_backup_db.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-class DropboxBackupDb(models.Model):
-    _name = 'dropbox.backup.db'
-    _description = 'Dropbox Backup Database'
-
-    name = fields.Char(string='Database Name', required=True)
