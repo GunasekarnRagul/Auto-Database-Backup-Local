adc01dd1ce253a66a9766f5bba546eb393d8d6e6
GunasekarnRagul <155290071+GunasekarnRagul@users.noreply.github.com>
2026-05-21 18:21:43 +0530

    Delete test_dbx_path.py

--- test_dbx_path.py
+++
@@ -1,4 +0,0 @@
-import re
-pattern = re.compile(r'(/(.|[\r\n])*)|(ns:[0-9]+(/(.|[\r\n])*)?)')
-print("Matches id:xxx/yyy:", bool(pattern.match("id:v415GCRH6a0AAAAAAAAADw/done")))
-print("Matches /root/yyy:", bool(pattern.match("/root/yyy")))
